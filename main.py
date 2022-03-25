b8ccdceca916da10d84fb715b148e60953d55b5e
shubham8patni <shubhampatni88@gmail.com>
2022-03-26 00:18:34 +0530

    march 25 post req

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template,request
+from sympy import re
 from connection1 import mydb
 
 mycursor = mydb.cursor()
@@ -10,35 +11,37 @@ app.secret_key = "andhisnameisjohncena"
 def home():
     return render_template("index.html") #"Department Names : FINANCE, AUDIT, MARKETING, PRODUCTION"        
 
-@app.route("/department/q=<name>")
+@app.route("/department/q=<name>", methods=["POST", ["GET"]])
 def listbydepartment(name):
-    valid = ["AUDIT", "MARKETING", "PRODUCTION", "FINANCE", "audit", "marketing", "production", "finance" ]
-    name = name.upper()
-    if name not in valid:
-        ans = {
-            "output": "Invalid department name!"
-        }
-        ans = jsonify(ans)
-        return  ans
+    if request.method == "GET":
+        valid = ["AUDIT", "MARKETING", "PRODUCTION", "FINANCE", "audit", "marketing", "production", "finance" ]
+        name = name.upper()
+        if name not in valid:
+            ans = {
+                "output": "Invalid department name!"
+            }
+            ans = jsonify(ans)
+            return  ans
+            
+        query = "select * from employees where dep_id = (select dep_id from departments where dep_name = '"+ name +"')"
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+        #print(result[0][0])
+        fin_ans = []
+        for i in result:
         
-    query = "select * from employees where dep_id = (select dep_id from departments where dep_name = '"+ name +"')"
-    mycursor.execute(query)
-    result = mycursor.fetchall()
-    #print(result[0][0])
-    fin_ans = []
-    for i in result:
-    
-        ans = {
-            "employee_id" : i[0],
-            "employee_name": i[1],
-            "designation": i[2],
-            "salary": i[5]
-        }
-        fin_ans.append(ans)
+            ans = {
+                "employee_id" : i[0],
+                "employee_name": i[1],
+                "designation": i[2],
+                "salary": i[5]
+            }
+            fin_ans.append(ans)
 
-    ans = jsonify(fin_ans)
-    return ans
-
+        ans = jsonify(fin_ans)
+        return ans
+    elif request.method=="POST":
+        print(name.json())
 
 if __name__ == "__main__":
     app.run(debug=True)
