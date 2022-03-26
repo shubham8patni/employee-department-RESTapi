2efbee57a57f30068ed9f6ebccf78e34183c5a68
shubham8patni <shubhampatni88@gmail.com>
2022-03-26 14:33:23 +0530

    march 26 post and all entries

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify, render_template,request
+from flask import Flask, jsonify, render_template,request, Response
 from connection1 import mydb
+import requests
+import json
 
 mycursor = mydb.cursor()
 
@@ -10,7 +12,56 @@ app.secret_key = "andhisnameisjohncena"
 def home():
     return render_template("index.html") #"Department Names : FINANCE, AUDIT, MARKETING, PRODUCTION"        
 
-@app.route("/department/q=<name>", methods=["POST", "GET"])
+
+@app.route("/call")
+def call():
+    
+    api_url = "http://employee-department-restapi.herokuapp.com/create"
+    #api_url = "http://127.0.0.1:5000/create"
+    create = {
+                "employee_id" : "7004",
+                "employee_name": "Bruce" ,
+                "designation": "salesman",
+                "salary": "65024",
+                "manager_id" : "68139",
+                "hire_date" : "18-5-21",
+                "department_name" : "Production"
+            }
+
+    headers =  {"Content-Type":"application/json"}
+    response = requests.post(api_url, data=json.dumps(create), headers=headers)
+    # print("2",response.json())
+    print("3 status of query : ", response.status_code)
+    return response.json()
+
+@app.route("/create", methods=["POST"])
+def create():
+    if request.method=="POST":
+        record = json.loads(request.data)
+        query="select * from employees where emp_id = '"+ record["employee_id"] +"'"
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+        if len(result)>0:
+            resp={
+                "error" : "Record alrady exists!"
+            }
+            return resp
+        else:
+            dep_name= record["department_name"].upper()
+            query1 = "select dep_id from departments where dep_name = '"+ dep_name +"'"
+            mycursor.execute(query1)
+            result = mycursor.fetchall()
+            print(result[0][0])
+
+            query = "INSERT INTO employees (emp_id, emp_name, job_name, manager_id, hire_date, salary, commission, dep_id) values(%s, %s, %s, %s, %s, %s, %s, %s)"
+            val = (record["employee_id"],record["employee_name"], record["designation"], record["manager_id"],record["hire_date"],record["salary"], "", result[0][0])
+            mycursor.execute(query, val)
+            mydb.commit()
+
+            return record,201, {'Content-Type': 'application/json'}
+
+
+@app.route("/department/q=<name>", methods=["GET"])
 def listbydepartment(name):
     if request.method == "GET":
         valid = ["AUDIT", "MARKETING", "PRODUCTION", "FINANCE", "audit", "marketing", "production", "finance" ]
@@ -25,8 +76,9 @@ def listbydepartment(name):
         query = "select * from employees where dep_id = (select dep_id from departments where dep_name = '"+ name +"')"
         mycursor.execute(query)
         result = mycursor.fetchall()
-        #print(result[0][0])
+        
         fin_ans = []
+        
         for i in result:
         
             ans = {
@@ -34,13 +86,46 @@ def listbydepartment(name):
                 "employee_name": i[1],
                 "designation": i[2],
                 "salary": i[5]
+                
             }
             fin_ans.append(ans)
 
         ans = jsonify(fin_ans)
         return ans
-    elif request.method=="POST":
-        return name.json()
+
+
+@app.route("/department", methods=["GET"])
+def listdepartment():
+    if request.method == "GET":
+            
+        query = "select * from employees order by dep_id ASC"
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+        #print(result[0][0])
+        fin_ans = []
+        
+        for i in result:
+            if(i[7]=="2001"):
+                dep = "AUDIT"
+            elif("3001"== i[7]):
+                dep = "MARKETING"
+            elif("4001"== i[7]):
+                dep = "PRODUCTION"
+            elif("1001"==i[7]):
+                 dep = "FINANCE"
+            ans = {
+                
+                "employee_id" : i[0],
+                "employee_name": i[1],
+                "designation": i[2],
+                "salary": i[5],
+                "department": dep
+            }
+            fin_ans.append(ans)
+
+        ans = jsonify(fin_ans)
+        return ans
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
--- trpi.py
+++ trpi.py
@@ -1,6 +1,7 @@
 import requests
-
-api_url = "http://employee-department-restapi.herokuapp.com/department/q=todo"
-todo = {"userId": 1, "title": "Buy milk", "completed": False}
-response = requests.post(api_url, json=todo)
+import json
+api_url = "http://employee-department-restapi.herokuapp.com/create"
+create = {"userId": 1, "title": "Buy milk", "completed": False}
+headers =  {"Content-Type":"application/json"}
+response = requests.post(api_url, data=json.dumps(create), headers=headers)
 print(response.json())
